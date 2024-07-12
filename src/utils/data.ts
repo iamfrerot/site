95e088c7a9869469355f9b65748c763139f003bb
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-07-12 16:30:55 +0200

    finished skills section (#12) done

---
+++ src/utils/data.ts
@@ -0,0 +1,25 @@
+export const projectsData = [
+ {
+  title: "Movie Land",
+  description:
+   "The Movie Land website is a movie search application. Users can search for movies, view details about them, and explore different categories",
+  link: "https://movielandfrerot.netlify.app",
+ },
+];
+
+export const skillsData = [
+ "HTML",
+ "CSS",
+ "javascript",
+ "typescript",
+ "react-js",
+ "react-native",
+ "node-js",
+ "express-js",
+ "firebase",
+ "mongodb",
+ "postgresql",
+ "git",
+ "redux",
+ "tailwind",
+];
