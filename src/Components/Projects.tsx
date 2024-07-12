123335d43d79fa526cd5c4f95eaeadd2799623d4
frerot <ntwalifrerotjacques@gmail.com>
2024-07-12 13:48:22 +0200

    finished the projects section

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,9 +1,21 @@
+import { HoverEffect } from "../ui/CardHoverEffect";
+
 const Projects = () => {
  return (
   <section id='projects' className='pt-10'>
    <h1 className='text-black-100 dark:text-white text-center text-3xl sm:text-4xl font-semibold '>
     Projects
    </h1>
+   <HoverEffect
+    items={[
+     {
+      title: "Movie Land",
+      description:
+       "The Movie Land website is a movie search application. Users can search for movies, view details about them, and explore different categories",
+      link: "https://movielandfrerot.netlify.app",
+     },
+    ]}
+   />
   </section>
  );
 };
