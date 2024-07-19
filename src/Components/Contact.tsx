123f5ab1e38084fcf1096a85426093257090c2a6
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-07-19 12:44:47 +0200

    Finished Experience Section (#14)

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -9,7 +9,8 @@ const Contact = () => {
    <form className='w-full md:max-w-[600px] py-4 sm:p-4 flex flex-col gap-y-8'>
     <p className='text-gray-600 font-extrabold text-base sm:text-lg'>
      having more questions? please fill form below and i'll get back to you
-     asap!.
+     asap!. or email:{" "}
+     <span className='text-secondary'>frerotntwali@gmail.com</span>
     </p>
     <input
      type='text'
