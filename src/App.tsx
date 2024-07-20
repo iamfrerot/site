7ace6951e882a3c1a0fc1193faeba401b7c7905c
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-07-20 21:12:12 +0200

    Finished the footer Section (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,20 @@ import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
+import Footer from "./Components/Footer";
 
 function App() {
  return (
   <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
    <main className='border border-white dark:border-black-100 bg-white dark:bg-black-100'>
-    <HeroHighlight containerClassName='bg-white dark:bg-black-100 px-4 md:px-10 pb-6'>
+    <HeroHighlight containerClassName='bg-white dark:bg-black-100 px-4 md:px-10 pb-6 overflow-x-hidden'>
      <Hero />
      <About />
      <Experience />
      <Projects />
      <Skills />
      <Contact />
+     <Footer />
     </HeroHighlight>
    </main>
   </ThemeProvider>
