69a5284f12852ba90d786503b6636a1f2cf273b6
frerot <ntwalifrerotjacques@gmail.com>
2024-07-21 11:13:04 +0200

    Added Contact functionality  - Added Sending Message to email  - Added react-hot-toast for toasting for any error or succes  - Used react-hook-form for form validation

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
 import Footer from "./Components/Footer";
+import { Toaster } from "react-hot-toast";
 
 function App() {
  return (
   <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
    <main className='border border-white dark:border-black-100 bg-white dark:bg-black-100'>
-    <HeroHighlight containerClassName='bg-white dark:bg-black-100 px-4 md:px-10 pb-6 overflow-x-hidden'>
+    <HeroHighlight containerClassName='bg-white dark:bg-black-100 px-4 md:px-10 py-4 overflow-x-hidden'>
      <Hero />
      <About />
      <Experience />
@@ -21,10 +22,19 @@ function App() {
      <Skills />
      <Contact />
      <Footer />
+     <Toaster
+      position='top-left'
+      toastOptions={{
+       style: {
+        background: "#363636",
+        color: "#fff",
+       },
+      }}
+     />
     </HeroHighlight>
    </main>
   </ThemeProvider>
  );
-}
+} 
 
 export default App;
