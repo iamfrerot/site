f06ce7a5edc923f21f8065d79ee3acc9e20a06e9
frerot <ntwalifrerotjacques@gmail.com>
2024-08-30 19:19:53 +0200

    update: Change the footer logo to logo-icon only

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,17 +3,17 @@ import { CiLinkedin } from "react-icons/ci";
 
 const Footer = () => {
  return (
-  <footer className="flex gap-6 justify-between  items-center mt-10 flex-wrap ">
-   <img src="/logo.svg" alt="Frerot Logo" className=" w-[90px]" />
-   <p className="text-black-100 dark:text-white sm:text-md">
-    &copy; {new Date().getFullYear()}, All Rights Reserved.
+  <footer className="flex justify-between items-center mt-10 gap-x-3 ">
+   <img src="/logoicon.svg" alt="Frerot Logo" className="w-8 md:w-10" />
+   <p className="text-black-100 dark:text-white text-sm sm:text-lg text-center">
+    &copy;{new Date().getFullYear()}, All Rights Reserved.
    </p>
    <div className="flex items-center justify-center gap-2">
     <a href="https://github.com/iamfrerot/" target="_blank">
-     <FaGithub className="text-black-100 dark:text-white text-[32px] hover:text-secondary hover:dark:text-secondary transition duration-500" />
+     <FaGithub className="text-black-100 dark:text-white text-[29px] sm:text-[32px] hover:text-secondary hover:dark:text-secondary transition duration-500" />
     </a>
     <a href="https://linkedin.com/in/ntwalifrerot" target="_blank">
-     <CiLinkedin className="text-black-100 dark:text-white text-[40px] hover:text-secondary hover:dark:text-secondary transition duration-500" />
+     <CiLinkedin className="text-black-100 dark:text-white text-[32px] sm:text-[35px] hover:text-secondary hover:dark:text-secondary transition duration-500" />
     </a>
    </div>
   </footer>
