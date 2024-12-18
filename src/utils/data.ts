66c93684aca7eda03f5828e813e4fa9bbd0aec9a
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-12-18 12:23:30 +0200

    tempory removed whitepace project
    Removed whitepace so that  can be ready
    added again after it's ready

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -13,13 +13,13 @@ export const projectsData = [
   link: "https://locus.frerot.dev/",
   stacks: ["React", "tailwind-css", "Framer", "Email-js", "React-Hook-Form"],
  },
- {
-  title: "whitepace",
-  description:
-   "a saas Landing Page for Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks, Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.",
-  link: "https://whitepace.frerot.dev",
-  stacks: ["next-js", "tailwind-css", "react"],
- },
+ // {
+ //  title: "whitepace",
+ //  description:
+ //   "a saas Landing Page for Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks, Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.",
+ //  link: "https://whitepace.frerot.dev",
+ //  stacks: ["next-js", "tailwind-css", "react"],
+ // },
 ];
 
 export const skillsData = [
